99a06588ecbc2e65944164d6012eea71b7ac653c
Selim <mustafa2001selim@gmail.com>
2024-09-02 13:54:00 +0300

    at the stage of working on tesseract OCR

---
+++ Form2.Designer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace QRCodeScanner
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(289, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(107, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 255);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "kamera";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(107, 360);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(289, 42);
+            this.textBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 436);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using Tesseract;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace QRCodeScanner
+{
+    public partial class Form2 : Form
+    {
+        string spesificQRCode = "https://monitoring.e-kassa.gov.az/#/index?doc=BdcQ9LPwqcNy2gSyQYnpgRvodhY14c8ig1zvMjDMPzyx";
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            for (int i = 0; i < filterInfoCollection.Count; i++)
+            {
+                comboBox1.Items.Add(filterInfoCollection[i].Name);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[comboBox1.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureDevice.IsRunning)
+            {
+                captureDevice.Stop();
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image!=null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pictureBox1.Image);
+                if (result != null)
+                {
+                    if (result.Text == spesificQRCode)
+
+                    {
+                        textBox1.Text = result.ToString();
+                        timer1.Stop();
+                        if (captureDevice.IsRunning)
+                        {
+                            captureDevice.Stop();
+                        }
+
+                        string imageUrl = "https://monitoring.e-kassa.gov.az/#/index?doc=BdcQ9LPwqcNy2gSyQYnpgRvodhY14c8ig1zvMjDMPzyx"; 
+                        string imagePath = await DownloadImage(imageUrl, "C:\\Users\\Selim\\Pictures\\receipt.png");
+
+                        string ocrText = PerformOCR(imagePath);
+                        var (total, tax, date, receiptNo) = ExtractData(ocrText);
+
+                        SaveToDatabase(total, tax, date, receiptNo);
+
+
+                        System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                        {
+                            FileName = result.Text,
+                            UseShellExecute = true
+                        });
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid QR Code! Please scan the correct QR code.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }  
+            }
+        }
+
+        public async Task<string> DownloadImage(string imageUrl, string savePath)
+        {
+            HttpClient client = new HttpClient();
+            var imageBytes = await client.GetByteArrayAsync(imageUrl);
+            await Task.Run(() => File.WriteAllBytes(savePath, imageBytes));
+            return savePath;
+        }
+
+        public string PerformOCR(string imagePath)
+        {
+            var ocrEngine = new TesseractEngine("C:\tesserract", "eng", EngineMode.Default);
+            using (var img = Pix.LoadFromFile(imagePath))
+            {
+                using (var page = ocrEngine.Process(img))
+                {
+                    return page.GetText();
+                }
+            }
+        }
+
+        public (string total, string tax, string date, string receiptNo) ExtractData(string ocrText)
+        {
+            string totalPattern = @"Total: \$(\d+\.\d{2})";
+            string taxPattern = @"Total Tax: \$(\d+\.\d{2})";
+            string datePattern = @"Date: (\d{2}/\d{2}/\d{4})";
+            string receiptPattern = @"Receipt No: (\d+)";
+
+            var totalMatch = Regex.Match(ocrText, totalPattern);
+            var taxMatch = Regex.Match(ocrText, taxPattern);
+            var dateMatch = Regex.Match(ocrText, datePattern);
+            var receiptMatch = Regex.Match(ocrText, receiptPattern);
+
+            return (
+                total: totalMatch.Groups[1].Value,
+                tax: taxMatch.Groups[1].Value,
+                date: dateMatch.Groups[1].Value,
+                receiptNo: receiptMatch.Groups[1].Value
+            );
+        }
+
+        public void SaveToDatabase(string total, string tax, string date, string receiptNo)
+        {
+            string connectionString = "\"Server=localhost;Database=receipt_infs;User Id=sa;Password=1;\"";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Receipts (Total, TotalTax, Date, ReceiptNo) VALUES (@total, @tax, @date, @receiptNo)";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@total", total);
+                command.Parameters.AddWithValue("@tax", tax);
+                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@receiptNo", receiptNo);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
